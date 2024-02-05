adad9118704fc5de715faf94f237cd40e45a8181
Robert Schmidt <robert.schmidt@openairinterface.org>
2024-02-05 17:38:46 +0100

    Request timing every slot
    This is to align with monolithic/FAPI, where we get slot indications in
    each slot. In the nFAPI, this is replaced by timing information, such
    that the VNF ticks automatically, and gets indications from the PNF on
    early/late messages. The timing information handling is not implemented
    in the present nFAPI implementation. Instead, we adopt the FAPI behavior
    and send timing information in every slot, abusing it to be a slot
    indication.

--- nfapi/oai_integration/nfapi_vnf.c
+++ nfapi/oai_integration/nfapi_vnf.c
@@ -1764,7 +1764,7 @@ void configure_nr_nfapi_vnf(char *vnf_addr, int vnf_p5_port, char *pnf_ip_addr, 
   vnf.p7_vnfs[0].timing_window = 30;
   vnf.p7_vnfs[0].periodic_timing_enabled = 0;
   vnf.p7_vnfs[0].aperiodic_timing_enabled = 0;
-  vnf.p7_vnfs[0].periodic_timing_period = 10;
+  vnf.p7_vnfs[0].periodic_timing_period = 1;
   vnf.p7_vnfs[0].config = nfapi_vnf_p7_config_create();
   NFAPI_TRACE(NFAPI_TRACE_INFO, "[VNF] %s() vnf.p7_vnfs[0].config:%p VNF ADDRESS:%s:%d\n", __FUNCTION__, vnf.p7_vnfs[0].config, vnf_addr, vnf_p5_port);
   strcpy(vnf.p7_vnfs[0].local_addr, vnf_addr);
