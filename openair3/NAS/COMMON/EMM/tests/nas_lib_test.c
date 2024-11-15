b6d0d22c878aa8fd1b6900321dfb5ae910ccfd20
Guido Casati <hello@guidocasati.com>
2024-11-15 16:02:28 +0100

    Add test for NAS Service Accept

--- openair3/NAS/COMMON/EMM/tests/nas_lib_test.c
+++ openair3/NAS/COMMON/EMM/tests/nas_lib_test.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <arpa/inet.h>
 #include "fgs_service_request.h"
+#include "fgmm_service_accept.h"
 #include "nr_nas_msg.h"
 
 void exit_function(const char *file, const char *function, const int line, const char *s, const int assert)
@@ -75,8 +76,74 @@ static void test_service_request(void)
               "test_service_request() failed: original and decoded messages do not match\n");
 }
 
+/**
+ * @brief Equality check for Service Accept (enc/dec)
+ */
+static bool eq_service_accept(const fgs_service_accept_msg_t *a, const fgs_service_accept_msg_t *b)
+{
+  for (int i = 0; i < a->num_psi_status; i++)
+    _NAS_EQ_CHECK_INT(a->psi_status[i], b->psi_status[i]);
+  for (int i = 0; i < a->num_psi_res; i++)
+    _NAS_EQ_CHECK_INT(a->psi_res[i], b->psi_res[i]);
+  _NAS_EQ_CHECK_INT(a->num_errors, b->num_errors);
+  _NAS_EQ_CHECK_INT(*a->t3448_value, *b->t3448_value);
+  return true;
+}
+
+/**
+ * @brief Test NAS Service Accept enc/dec
+ */
+static void test_service_accept(void)
+{
+  // Dummy NAS Service Accept message
+  fgs_service_accept_msg_t orig = {
+      .num_psi_status = 3,
+      .num_psi_res = 2,
+      .num_errors = 1,
+      .t3448_value = malloc(sizeof(*orig.t3448_value)),
+  };
+  orig.psi_status[0] = PDU_SESSION_ACTIVE;
+  orig.psi_status[1] = PDU_SESSION_INACTIVE;
+  orig.psi_status[2] = PDU_SESSION_ACTIVE;
+  orig.psi_res[0] = REACTIVATION_FAILED;
+  orig.psi_res[1] = REACTIVATION_SUCCESS;
+  orig.cause[0].cause = Illegal_UE;
+  orig.cause[0].pdu_session_id = 1;
+  *orig.t3448_value = 100;
+
+  // Expected encoded data
+  uint8_t dummy_enc[] = {IEI_PDU_SESSION_STATUS, 0x02, 0x0a, 0x00,
+                         IEI_PDU_SESSION_REACT_RESULT, 0x02, 0x02, 0x00,
+                         IEI_PDU_SESSION_REACT_RESULT_ERROR_CAUSE, 0x00, 0x00, 0x01, 0x03,
+                         IEI_T3448_VALUE, 0x01, 0x64};
+  uint16_t len_status = htons(2);
+  memcpy(dummy_enc + 9, &len_status, sizeof(len_status)); // length payload Reactivation Result Cause
+
+  // Buffer
+  uint8_t buffer[64];
+  memset(buffer, 0, sizeof(buffer));
+
+  // Encode NAS Service Accept
+  int encoded_length = encode_fgs_service_accept(buffer, &orig, sizeof(buffer));
+  AssertFatal(encoded_length >= 0, "encode_fgs_service_accept() failed\n");
+
+  // Compare the raw encoded buffer with expected encoded data
+  AssertFatal(memcmp(buffer, dummy_enc, encoded_length) == 0, "Encoding mismatch!\n");
+
+  // Decode NAS Service Accept
+  fgs_service_accept_msg_t dec = {0};
+  int decoded_length = decode_fgs_service_accept(&dec, buffer, encoded_length);
+  AssertFatal(decoded_length >= 0, "decode_fgs_service_accept() failed\n");
+
+  // Compare original and decoded messages
+  AssertFatal(eq_service_accept(&orig, &dec), "test_service_accept() failed: original and decoded messages do not match\n");
+  free_fgs_service_accept(&dec);
+  free_fgs_service_accept(&orig);
+}
+
 int main()
 {
   test_service_request();
+  test_service_accept();
   return 0;
 }
