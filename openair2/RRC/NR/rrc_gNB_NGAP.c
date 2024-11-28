turkeryilmaz/openairinterface5g

Refactor and cleanup rrc_gNB_send_NGAP_NAS_FIRST_REQ
* improve readability and maintainability

openair2/RRC/NR/rrc_gNB_NGAP.c
@@ -143,12 +143,19 @@ void nr_rrc_pdcp_config_security(gNB_RRC_UE_t *UE, bool enable_ciphering)
   nr_pdcp_config_set_security(UE->rrc_ue_id, DCCH, true, &security_parameters);
 }
 
-//------------------------------------------------------------------------------
-/*
-* Initial UE NAS message on S1AP.
-*/
+// Process AMF Identifier and fill GUAMI struct members
+static void process_amf_Identifier(uint32_t amf_Id, guami_t *guami)
+{
+  guami->amf_region_id = (amf_Id >> 16) & 0xff;
+  guami->amf_set_id = (amf_Id >> 6) & 0x3ff;
+  guami->amf_pointer = amf_Id & 0x3f;
+}
+
+/**
+ * Initial UE Message (NAS) on N2
+ * @brief Forward the first uplink NAS message to the AMF
+ */
 void rrc_gNB_send_NGAP_NAS_FIRST_REQ(gNB_RRC_INST *rrc, gNB_RRC_UE_t *UE, NR_RRCSetupComplete_IEs_t *rrcSetupComplete)
-//------------------------------------------------------------------------------
 {
   MessageDef *message_p = itti_alloc_new_message(TASK_RRC_GNB, rrc->module_id, NGAP_NAS_FIRST_REQ);
   ngap_nas_first_req_t *req = &NGAP_NAS_FIRST_REQ(message_p);
@@ -165,9 +172,6 @@ void rrc_gNB_send_NGAP_NAS_FIRST_REQ(gNB_RRC_INST *rrc, gNB_RRC_UE_t *UE, NR_RRC
   req->nas_pdu.buffer = malloc(req->nas_pdu.length);
   AssertFatal(req->nas_pdu.buffer != NULL, "out of memory\n");
   memcpy(req->nas_pdu.buffer, rrcSetupComplete->dedicatedNAS_Message.buf, req->nas_pdu.length);
-  // extract_imsi(NGAP_NAS_FIRST_REQ (message_p).nas_pdu.buffer,
-  //              NGAP_NAS_FIRST_REQ (message_p).nas_pdu.length,
-  //              ue_context_pP);
 
   /* selected_plmn_identity: IE is 1-based, convert to 0-based (C array) */
   int selected_plmn_identity = rrcSetupComplete->selectedPLMN_Identity - 1;
@@ -184,24 +188,22 @@ void rrc_gNB_send_NGAP_NAS_FIRST_REQ(gNB_RRC_INST *rrc, gNB_RRC_UE_t *UE, NR_RRC
     req->ue_identity.s_tmsi.amf_pointer = UE->Initialue_identity_5g_s_TMSI.amf_pointer;
     req->ue_identity.s_tmsi.m_tmsi = UE->Initialue_identity_5g_s_TMSI.fiveg_tmsi;
   } else if (rrcSetupComplete->registeredAMF != NULL) {
+    /**
+     * Fetch the AMF-Identifier from the registeredAMF IE
+     * The IE AMF-Identifier (AMFI) comprises of an AMF Region ID (8b),
+     * an AMF Set ID (10b) and an AMF Pointer (6b)
+     * as specified in TS 23.003 [21], clause 2.10.1. */
     NR_RegisteredAMF_t *r_amf = rrcSetupComplete->registeredAMF;
     req->ue_identity.presenceMask = NGAP_UE_IDENTITIES_guami;
-
-    /* The IE AMF-Identifier (AMFI) comprises of an AMF Region ID (8b), an AMF Set ID (10b) and an AMF Pointer (6b) as specified in TS 23.003 [21], clause 2.10.1. */
     uint32_t amf_Id = BIT_STRING_to_uint32(&r_amf->amf_Identifier);
-    req->ue_identity.guami.amf_region_id = (amf_Id >> 16) & 0xff;
-    req->ue_identity.guami.amf_set_id = (amf_Id >> 6) & 0x3ff;
-    req->ue_identity.guami.amf_pointer = amf_Id & 0x3f;
-
-    UE->ue_guami.amf_region_id = req->ue_identity.guami.amf_region_id;
-    UE->ue_guami.amf_set_id = req->ue_identity.guami.amf_set_id;
-    UE->ue_guami.amf_pointer = req->ue_identity.guami.amf_pointer;
-
+    process_amf_Identifier(amf_Id, &req->ue_identity.guami);
+    process_amf_Identifier(amf_Id, &UE->ue_guami);
     LOG_I(NGAP,
-          "Build NGAP_NAS_FIRST_REQ adding in s_TMSI: GUAMI amf_set_id %u amf_region_id %u ue %x\n",
+          "GUAMI in NGAP_NAS_FIRST_REQ (UE %04x): AMF Set ID %u, Region ID %u, Pointer %u\n",
+          UE->rnti,
           req->ue_identity.guami.amf_set_id,
           req->ue_identity.guami.amf_region_id,
-          UE->rnti);
+          req->ue_identity.guami.amf_pointer);
   } else {
     req->ue_identity.presenceMask = NGAP_UE_IDENTITIES_NONE;
   }

saffanazyan07/zy-OAI

openair2/RRC/NR/rrc_gNB_NGAP.c
@@ -151,12 +151,19 @@ void nr_rrc_pdcp_config_security(gNB_RRC_UE_t *UE, bool enable_ciphering)
   nr_pdcp_config_set_security(UE->rrc_ue_id, DL_SCH_LCID_DCCH, true, &security_parameters);
 }
 
-//------------------------------------------------------------------------------
-/*
-* Initial UE NAS message on S1AP.
-*/
+// Process AMF Identifier and fill GUAMI struct members
+static void process_amf_Identifier(uint32_t amf_Id, nr_guami_t *guami)
+{
+  guami->amf_region_id = (amf_Id >> 16) & 0xff;
+  guami->amf_set_id = (amf_Id >> 6) & 0x3ff;
+  guami->amf_pointer = amf_Id & 0x3f;
+}
+
+/**
+ * Initial UE Message (NAS) on N2
+ * @brief Forward the first uplink NAS message to the AMF
+ */
 void rrc_gNB_send_NGAP_NAS_FIRST_REQ(gNB_RRC_INST *rrc, gNB_RRC_UE_t *UE, NR_RRCSetupComplete_IEs_t *rrcSetupComplete)
-//------------------------------------------------------------------------------
 {
   MessageDef *message_p = itti_alloc_new_message(TASK_RRC_GNB, rrc->module_id, NGAP_NAS_FIRST_REQ);
   ngap_nas_first_req_t *req = &NGAP_NAS_FIRST_REQ(message_p);
@@ -173,9 +180,6 @@ void rrc_gNB_send_NGAP_NAS_FIRST_REQ(gNB_RRC_INST *rrc, gNB_RRC_UE_t *UE, NR_RRC
   req->nas_pdu.buffer = malloc(req->nas_pdu.length);
   AssertFatal(req->nas_pdu.buffer != NULL, "out of memory\n");
   memcpy(req->nas_pdu.buffer, rrcSetupComplete->dedicatedNAS_Message.buf, req->nas_pdu.length);
-  // extract_imsi(NGAP_NAS_FIRST_REQ (message_p).nas_pdu.buffer,
-  //              NGAP_NAS_FIRST_REQ (message_p).nas_pdu.length,
-  //              ue_context_pP);
 
   /* selected_plmn_identity: IE is 1-based, convert to 0-based (C array) */
   int selected_plmn_identity = rrcSetupComplete->selectedPLMN_Identity - 1;
@@ -192,24 +196,22 @@ void rrc_gNB_send_NGAP_NAS_FIRST_REQ(gNB_RRC_INST *rrc, gNB_RRC_UE_t *UE, NR_RRC
     req->ue_identity.s_tmsi.amf_pointer = UE->Initialue_identity_5g_s_TMSI.amf_pointer;
     req->ue_identity.s_tmsi.m_tmsi = UE->Initialue_identity_5g_s_TMSI.fiveg_tmsi;
   } else if (rrcSetupComplete->registeredAMF != NULL) {
+    /**
+     * Fetch the AMF-Identifier from the registeredAMF IE
+     * The IE AMF-Identifier (AMFI) comprises of an AMF Region ID (8b),
+     * an AMF Set ID (10b) and an AMF Pointer (6b)
+     * as specified in TS 23.003 [21], clause 2.10.1. */
     NR_RegisteredAMF_t *r_amf = rrcSetupComplete->registeredAMF;
     req->ue_identity.presenceMask = NGAP_UE_IDENTITIES_guami;
-
-    /* The IE AMF-Identifier (AMFI) comprises of an AMF Region ID (8b), an AMF Set ID (10b) and an AMF Pointer (6b) as specified in TS 23.003 [21], clause 2.10.1. */
     uint32_t amf_Id = BIT_STRING_to_uint32(&r_amf->amf_Identifier);
-    req->ue_identity.guami.amf_region_id = (amf_Id >> 16) & 0xff;
-    req->ue_identity.guami.amf_set_id = (amf_Id >> 6) & 0x3ff;
-    req->ue_identity.guami.amf_pointer = amf_Id & 0x3f;
-
-    UE->ue_guami.amf_region_id = req->ue_identity.guami.amf_region_id;
-    UE->ue_guami.amf_set_id = req->ue_identity.guami.amf_set_id;
-    UE->ue_guami.amf_pointer = req->ue_identity.guami.amf_pointer;
-
+    process_amf_Identifier(amf_Id, &req->ue_identity.guami);
+    process_amf_Identifier(amf_Id, &UE->ue_guami);
     LOG_I(NGAP,
-          "Build NGAP_NAS_FIRST_REQ adding in s_TMSI: GUAMI amf_set_id %u amf_region_id %u ue %x\n",
+          "GUAMI in NGAP_NAS_FIRST_REQ (UE %04x): AMF Set ID %u, Region ID %u, Pointer %u\n",
+          UE->rnti,
           req->ue_identity.guami.amf_set_id,
           req->ue_identity.guami.amf_region_id,
-          UE->rnti);
+          req->ue_identity.guami.amf_pointer);
   } else {
     req->ue_identity.presenceMask = NGAP_UE_IDENTITIES_NONE;
   }