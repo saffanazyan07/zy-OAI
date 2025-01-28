AntonioFern2024/oai

Added LatSeq points to layers

openair1/SCHED_NR/phy_procedures_nr_gNB.c
@@ -39,6 +39,7 @@
 #include "nfapi/oai_integration/vendor_ext.h"
 #include "assertions.h"
 #include <time.h>
+#include "common/utils/LATSEQ/latseq.h"
 
 //#define DEBUG_RXDATA
 //#define SRS_IND_DEBUG
@@ -211,6 +212,7 @@ void phy_procedures_gNB_TX(processingData_L1tx_t *msgTx,
   if (msgTx->num_pdsch_slot > 0) {
     VCD_SIGNAL_DUMPER_DUMP_FUNCTION_BY_NAME(VCD_SIGNAL_DUMPER_FUNCTIONS_GENERATE_DLSCH,1);
     LOG_D(PHY, "PDSCH generation started (%d) in frame %d.%d\n", msgTx->num_pdsch_slot,frame,slot);
+    LATSEQ_P("D mac.dci--phy.crc", "::fm%u.sl%u", frame, slot);
     nr_generate_pdsch(msgTx, frame, slot);
     VCD_SIGNAL_DUMPER_DUMP_FUNCTION_BY_NAME(VCD_SIGNAL_DUMPER_FUNCTIONS_GENERATE_DLSCH,0);
   }
@@ -258,6 +260,7 @@ void phy_procedures_gNB_TX(processingData_L1tx_t *msgTx,
   }
 
   VCD_SIGNAL_DUMPER_DUMP_FUNCTION_BY_NAME(VCD_SIGNAL_DUMPER_FUNCTIONS_PHY_PROCEDURES_gNB_TX + gNB->CC_id, 0);
+  LATSEQ_P("D phy.rotated--phy.ofdmidft", "::fm%u.sl%u", frame, slot);
 }
 
 static void nr_postDecode(PHY_VARS_gNB *gNB, notifiedFIFO_elt_t *req)
@@ -277,6 +280,7 @@ static void nr_postDecode(PHY_VARS_gNB *gNB, notifiedFIFO_elt_t *req)
         decodeSuccess ? "Decoded Successfully" : "Decoding Unsuccessful");
 
   if (decodeSuccess) {
+    LATSEQ_P("U phy.CBdec--phy.TBdec","::fm%u.sl%u.hqpid%u.ldpciter%u.segmentnb%u.rnti%u", ulsch->frame, ulsch->slot, rdata->harq_pid, rdata->decodeIterations, r, pusch_pdu->rnti);
     memcpy(ulsch_harq->b + rdata->offset, ulsch_harq->c[r], rdata->Kr_bytes - (ulsch_harq->F >> 3) - ((ulsch_harq->C > 1) ? 3 : 0));
 
   } else {
@@ -308,6 +312,7 @@ static void nr_postDecode(PHY_VARS_gNB *gNB, notifiedFIFO_elt_t *req)
             ulsch_harq->round,
             ulsch_harq->TBS,
             rdata->decodeIterations);
+      LATSEQ_P("U phy.TBdec--phy.srs", "::fm%u.sl%u.hqpid%u.hqround%u.rnti%u.CBbits%u.Fbits%u.TBS%u", ulsch->frame, ulsch->slot, rdata->harq_pid, ulsch_harq->round, pusch_pdu->rnti, ulsch_harq->K, ulsch_harq->F, ulsch_harq->TBS);
       nr_fill_indication(gNB, ulsch->frame, ulsch->slot, rdata->ulsch_id, rdata->harq_pid, 0, 0);
       LOG_D(PHY, "ULSCH received ok \n");
       ulsch->active = false;
@@ -332,6 +337,12 @@ static void nr_postDecode(PHY_VARS_gNB *gNB, notifiedFIFO_elt_t *req)
       nr_fill_indication(gNB, ulsch->frame, ulsch->slot, rdata->ulsch_id, rdata->harq_pid, 1, 0);
       ulsch->handled = 1;
       LOG_D(PHY, "ULSCH %d in error\n",rdata->ulsch_id);
+      if (ulsch_harq->round == 3) {
+        LATSEQ_P("U phy.CBdec--phy.retxdrop","::fm%u.sl%u.hqpid%u.segmentnb%u.rnti%u", ulsch->frame, ulsch->slot, rdata->harq_pid, r, pusch_pdu->rnti);
+      } else {
+        LATSEQ_P("U phy.CBdec--phy.retxdecfail","::fm%u.sl%u.hqpid%u.segmentnb%u.rnti%u", ulsch->frame, ulsch->slot, rdata->harq_pid, r, pusch_pdu->rnti, ulsch_harq->round);
+        LATSEQ_P("U phy.retxdecfail--phy.prachpucch","::hqpid%u.rnti%u", rdata->harq_pid, pusch_pdu->rnti);
+      }
       //      dumpsig=1;
     }
     ulsch->last_iteration_cnt = rdata->decodeIterations;
@@ -1134,6 +1145,7 @@ int phy_procedures_gNB_uespec_RX(PHY_VARS_gNB *gNB, int frame_rx, int slot_rx)
         srs->active = 0;
 
         stop_meas(&gNB->rx_srs_stats);
+        LATSEQ_P("U phy.srs--phy.rachuci", "::fm%u.sl%u.rnti%u", frame_rx, slot_rx, srs_pdu->rnti);
       }
     }
   }

openair2/LAYER2/nr_pdcp/nr_pdcp_entity.c
@@ -32,6 +32,7 @@
 #include "nr_pdcp_sdu.h"
 
 #include "LOG/log.h"
+#include "common/utils/LATSEQ/latseq.h"
 
 /**
  * @brief returns the maximum PDCP PDU size
@@ -160,6 +161,7 @@ static void nr_pdcp_entity_recv_pdu(nr_pdcp_entity_t *entity,
     LOG_W(PDCP, "discard NR PDU rcvd_count=%d, entity->rx_deliv %d,sdu_in_list %d\n", rcvd_count,entity->rx_deliv,nr_pdcp_sdu_in_list(entity->rx_list,rcvd_count));
     entity->stats.rxpdu_dd_pkts++;
     entity->stats.rxpdu_dd_bytes += size;
+    LATSEQ_P("U pdcp.decoded--pdcp.discarded.rcvdsmallerdeliv", "::sn%u.pdcpsdusize%u", rcvd_sn, size-header_size-integrity_size);
 
     return;
   }
@@ -182,6 +184,7 @@ static void nr_pdcp_entity_recv_pdu(nr_pdcp_entity_t *entity,
     uint32_t count = entity->rx_deliv;
     while (entity->rx_list != NULL && count == entity->rx_list->count) {
       nr_pdcp_sdu_t *cur = entity->rx_list;
+      LATSEQ_P("U pdcp.decoded--sdap.sdu", "::sn%u.PSbuf%u.pdcpsdusize%u", rcvd_sn, cur->buffer, sdu->size);
       entity->deliver_sdu(entity->deliver_sdu_data, entity,
                           cur->buffer, cur->size,
                           &cur->msg_integrity);
@@ -294,6 +297,7 @@ static int nr_pdcp_entity_process_sdu(nr_pdcp_entity_t *entity,
                    entity->rb_id, count, entity->is_gnb ? 1 : 0);
   }
 
+  LATSEQ_P("D pdcp.hdr--pdcp.enqueue", "::SPbuf%u.sn%u.Pbuf%u", buffer, sn, buf);
   entity->tx_next++;
 
   entity->stats.txpdu_pkts++;
@@ -418,6 +422,8 @@ static void check_t_reordering(nr_pdcp_entity_t *entity)
   /* deliver all SDUs with count < rx_reord */
   while (entity->rx_list != NULL && entity->rx_list->count < entity->rx_reord) {
     nr_pdcp_sdu_t *cur = entity->rx_list;
+    LATSEQ_P("U pdcp.decoded--pdcp.outoforderdeliv", "::sn%u.pdcpsdusize%u", cur->count, cur->size);
+    LATSEQ_P("U pdcp.outoforderdeliv--sdap.sdu", "::sn%u.PSbuf%u", cur->count, cur->buffer);
     entity->deliver_sdu(entity->deliver_sdu_data, entity,
                         cur->buffer, cur->size,
                         &cur->msg_integrity);
@@ -432,6 +438,8 @@ static void check_t_reordering(nr_pdcp_entity_t *entity)
   count = entity->rx_reord;
   while (entity->rx_list != NULL && count == entity->rx_list->count) {
     nr_pdcp_sdu_t *cur = entity->rx_list;
+    LATSEQ_P("U pdcp.decoded--pdcp.reorderdeliv", "::sn%u.pdcpsdusize%u", cur->count, cur->size);
+    LATSEQ_P("U pdcp.reorderdeliv--sdap.sdu", "::sn%u.PSbuf%u", cur->count, cur->buffer);
     entity->deliver_sdu(entity->deliver_sdu_data, entity,
                         cur->buffer, cur->size,
                         &cur->msg_integrity);

saffanazyan07/zy-OAI

openair1/SCHED_NR/phy_procedures_nr_gNB.c
@@ -40,6 +40,7 @@
 #include "assertions.h"
 #include <time.h>
 #include <stdint.h>
+#include "common/utils/LATSEQ/latseq.h"
 
 //#define DEBUG_RXDATA
 //#define SRS_IND_DEBUG
@@ -248,6 +249,7 @@ void phy_procedures_gNB_TX(processingData_L1tx_t *msgTx,
   if (msgTx->num_pdsch_slot > 0) {
     VCD_SIGNAL_DUMPER_DUMP_FUNCTION_BY_NAME(VCD_SIGNAL_DUMPER_FUNCTIONS_GENERATE_DLSCH,1);
     LOG_D(PHY, "PDSCH generation started (%d) in frame %d.%d\n", msgTx->num_pdsch_slot,frame,slot);
+    LATSEQ_P("D mac.dci--phy.crc", "::fm%u.sl%u", frame, slot);
     nr_generate_pdsch(msgTx, frame, slot);
     VCD_SIGNAL_DUMPER_DUMP_FUNCTION_BY_NAME(VCD_SIGNAL_DUMPER_FUNCTIONS_GENERATE_DLSCH,0);
   }
@@ -306,6 +308,7 @@ void phy_procedures_gNB_TX(processingData_L1tx_t *msgTx,
   }
 
   VCD_SIGNAL_DUMPER_DUMP_FUNCTION_BY_NAME(VCD_SIGNAL_DUMPER_FUNCTIONS_PHY_PROCEDURES_gNB_TX + gNB->CC_id, 0);
+  LATSEQ_P("D phy.rotated--phy.ofdmidft", "::fm%u.sl%u", frame, slot);
 }
 
 static int nr_ulsch_procedures(PHY_VARS_gNB *gNB, int frame_rx, int slot_rx, bool *ulsch_to_decode, NR_UL_IND_t *UL_INFO)
@@ -417,6 +420,7 @@ static int nr_ulsch_procedures(PHY_VARS_gNB *gNB, int frame_rx, int slot_rx, boo
             ulsch_harq->round,
             ulsch_harq->TBS,
             ulsch->max_ldpc_iterations);
+      LATSEQ_P("U phy.TBdec--phy.srs", "::fm%u.sl%u.hqpid%u.hqround%u.rnti%u.CBbits%u.Fbits%u.TBS%u", ulsch->frame, ulsch->slot, ulsch->harq_pid, ulsch_harq->round, pusch_pdu->rnti, ulsch_harq->K, ulsch_harq->F, ulsch_harq->TBS);
       nr_fill_indication(gNB, ulsch->frame, ulsch->slot, ULSCH_id, ulsch->harq_pid, 0, 0, crc, pdu);
       LOG_D(PHY, "ULSCH received ok \n");
       ulsch->active = false;
@@ -1122,6 +1126,7 @@ int phy_procedures_gNB_uespec_RX(PHY_VARS_gNB *gNB, int frame_rx, int slot_rx, N
     }
   }
 
+  LATSEQ_P("U phy.srs--phy.rachuci", "::fm%u.sl%u", frame_rx, slot_rx);
   stop_meas(&gNB->phy_proc_rx);
 
   if (pucch_decode_done || pusch_decode_done) {

openair2/LAYER2/nr_pdcp/nr_pdcp_entity.c
@@ -32,6 +32,7 @@
 #include "nr_pdcp_sdu.h"
 
 #include "LOG/log.h"
+#include "common/utils/LATSEQ/latseq.h"
 
 /**
  * @brief returns the maximum PDCP PDU size
@@ -160,6 +161,7 @@ static void nr_pdcp_entity_recv_pdu(nr_pdcp_entity_t *entity,
     LOG_W(PDCP, "discard NR PDU rcvd_count=%d, entity->rx_deliv %d,sdu_in_list %d\n", rcvd_count,entity->rx_deliv,nr_pdcp_sdu_in_list(entity->rx_list,rcvd_count));
     entity->stats.rxpdu_dd_pkts++;
     entity->stats.rxpdu_dd_bytes += size;
+    LATSEQ_P("U pdcp.decoded--pdcp.discarded.rcvdsmallerdeliv", "::sn%u.pdcpsdusize%u", rcvd_sn, size-header_size-integrity_size);
 
     return;
   }
@@ -182,6 +184,7 @@ static void nr_pdcp_entity_recv_pdu(nr_pdcp_entity_t *entity,
     uint32_t count = entity->rx_deliv;
     while (entity->rx_list != NULL && count == entity->rx_list->count) {
       nr_pdcp_sdu_t *cur = entity->rx_list;
+      LATSEQ_P("U pdcp.decoded--sdap.sdu", "::sn%u.PSbuf%u.pdcpsdusize%u", rcvd_sn, cur->buffer, sdu->size);
       entity->deliver_sdu(entity->deliver_sdu_data, entity,
                           cur->buffer, cur->size,
                           &cur->msg_integrity);
@@ -294,6 +297,7 @@ static int nr_pdcp_entity_process_sdu(nr_pdcp_entity_t *entity,
                    entity->rb_id, count, entity->is_gnb ? 1 : 0);
   }
 
+  LATSEQ_P("D pdcp.hdr--rlc.seg", "::SPbuf%u.sn%u.Pbuf%u", buffer, sn, buf);
   entity->tx_next++;
 
   entity->stats.txpdu_pkts++;
@@ -418,6 +422,8 @@ static void check_t_reordering(nr_pdcp_entity_t *entity)
   /* deliver all SDUs with count < rx_reord */
   while (entity->rx_list != NULL && entity->rx_list->count < entity->rx_reord) {
     nr_pdcp_sdu_t *cur = entity->rx_list;
+    LATSEQ_P("U pdcp.decoded--pdcp.outoforderdeliv", "::sn%u.pdcpsdusize%u", cur->count, cur->size);
+    LATSEQ_P("U pdcp.outoforderdeliv--sdap.sdu", "::sn%u.PSbuf%u", cur->count, cur->buffer);
     entity->deliver_sdu(entity->deliver_sdu_data, entity,
                         cur->buffer, cur->size,
                         &cur->msg_integrity);
@@ -432,6 +438,8 @@ static void check_t_reordering(nr_pdcp_entity_t *entity)
   count = entity->rx_reord;
   while (entity->rx_list != NULL && count == entity->rx_list->count) {
     nr_pdcp_sdu_t *cur = entity->rx_list;
+    LATSEQ_P("U pdcp.decoded--pdcp.reorderdeliv", "::sn%u.pdcpsdusize%u", cur->count, cur->size);
+    LATSEQ_P("U pdcp.reorderdeliv--sdap.sdu", "::sn%u.PSbuf%u", cur->count, cur->buffer);
     entity->deliver_sdu(entity->deliver_sdu_data, entity,
                         cur->buffer, cur->size,
                         &cur->msg_integrity);